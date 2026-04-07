5e824cf8fcdc5b58f95e9a35415947847a4d50a4
binseoyun <sunnybin04@gmail.com>
2026-04-07 21:23:03 +0900

    백엔드 개발 환경 세팅

---
+++ backend/src/main/kotlin/com/bifriends/backend/BiFriendsBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.bifriends.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BiFriendsBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<BiFriendsBackendApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/bifriends/backend/BiFriendsBackendApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.bifriends.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BiFriendsBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
